a30503ffce9c5e9964d27b0b12faeb343ca389f5
mciganovic <mciganovic@griddynamics.com>
2025-08-25 14:08:42 +0200

    No favorites handler added

--- src/pages/FavoritesPage.vue
+++ src/pages/FavoritesPage.vue
@@ -1,5 +1,6 @@
 <template>
   <Layout>
+    <DataStateHandler :data-exists="favourites.length" :is-loading="false" :is-error="false" no-data-message="You haven't added any characters to your favorites yet. Visit the character list to add some!" :back-button-visible="true">
     <div class="container-fluid">
       <div class="card-container">
         <CharacterCard
@@ -9,6 +10,7 @@
         />
       </div>
     </div>
+    </DataStateHandler>
   </Layout>
 </template>
 <script setup lang="ts">
@@ -16,6 +18,7 @@ import Layout from "../components/layout/Layout.vue";
 import CharacterCard from "../components/CharacterCard.vue";
 import {useFavorites} from "../composables/useFavorites.ts";
 import {storeToRefs} from "pinia";
+import DataStateHandler from "../components/layout/DataStateHandler.vue";
 
 const store = useFavorites()
 const { favourites } = storeToRefs(store)
